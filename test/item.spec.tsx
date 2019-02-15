cf925814764f09554356487f633eda476414a5bd
William Rozin Gaspar <williamrozingaspar@gmail.com>
2019-02-14 19:06:08 -0500

    Item.tsx test

--- test/item.spec.tsx
+++ test/item.spec.tsx
@@ -3,10 +3,17 @@ import React from 'react'
 
 import Item from '../src/components/rates/list/Item'
 
-describe('hello', () => {
-    it('should eita', () => {
-        const item = shallow(<Item index={ 1 }>EITA</Item>)
-        expect(item.contains('EITA')).toBeTruthy()
+describe('Item component', () => {
+    it('should render an item with an even index', () => {
+        const item = shallow(<Item index={ 0 }>Even item</Item>)
+        expect(item.contains('Even item')).toBeTruthy()
+        expect(item.contains('"backgroundColor": "#EEEEEE"'))
+        expect(item).toMatchSnapshot()
+    })
+    it('Should render an Item with an odd index', () => {
+        const item = shallow(<Item index={ 1 }>Odd item</Item>)
+        expect(item.contains('Odd item')).toBeTruthy()
+        expect(item.contains('"backgroundColor": "#F5F5F5"'))
         expect(item).toMatchSnapshot()
     })
 })
